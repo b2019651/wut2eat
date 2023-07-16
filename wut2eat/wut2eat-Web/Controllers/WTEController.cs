cb73564ef94a4487fd6d542eeda86653e650f171
b2019651 <b2019651@gmail.com>
2023-07-16 21:16:12 +0800

    新增第一張 View
    - 安裝 Nuget bootstrap

--- wut2eat/wut2eat-Web/Controllers/WTEController.cs
+++ wut2eat/wut2eat-Web/Controllers/WTEController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using wut2eat_Web.Models;
 
 namespace wut2eat_Web.Controllers
 {
     public class WTEController : Controller
     {
+        readonly dbWhatToEatEntities _db = new dbWhatToEatEntities();
+
         // GET: WTE
         public ActionResult Index()
         {
-            return View();
+            var records = _db.tWhatToEatList.OrderBy(r => Guid.NewGuid()).FirstOrDefault();
+            return View(records);
         }
     }
 }
